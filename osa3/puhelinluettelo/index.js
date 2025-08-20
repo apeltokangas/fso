92635bc8fd5439b8a4e04097ebc9a7768dba2678
Peltokangas Aapo Tapio <aapo.t.peltokangas@student.jyu.fi>
2025-08-20 15:18:23 +0300

    turha hello world pois

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -32,10 +32,6 @@ morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
-
 app.get('/api/persons', (request, response) => {
   response.json(notes)
 })
