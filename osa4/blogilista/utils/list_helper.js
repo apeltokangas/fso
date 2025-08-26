9dd9508737f019578f7ba690a804284212301db9
Peltokangas Aapo Tapio <aapo.t.peltokangas@student.jyu.fi>
2025-08-26 19:54:11 +0300

    4.6 ja 4.7

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -23,11 +23,60 @@ const favoriteBlog = (blogs) => {
     return favorite
 }
 
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0) return null
+    const counts = {}
+
+    blogs.forEach(blog => {
+        counts[blog.author] = (counts[blog.author] || 0) + 1
+    })
+
+    let maxAuthor = null
+    let maxBlogs = 0
+
+    for (const author in counts) {
+        if (counts[author] > maxBlogs) {
+            maxAuthor = author
+            maxBlogs = counts[author]
+        }
+    }
+    return {
+        author: maxAuthor,
+        blogs: maxBlogs
+    }
+}
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) return null
+
+  const likeCounts = {}
+
+  blogs.forEach(blog => {
+    likeCounts[blog.author] = (likeCounts[blog.author] || 0) + blog.likes
+  })
+
+  let maxAuthor = null
+  let maxLikes = 0
+
+  for (const author in likeCounts) {
+    if (likeCounts[author] > maxLikes) {
+      maxAuthor = author
+      maxLikes = likeCounts[author]
+    }
+  }
+
+  return {
+    author: maxAuthor,
+    likes: maxLikes
+  }
+}
 
 
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
 
